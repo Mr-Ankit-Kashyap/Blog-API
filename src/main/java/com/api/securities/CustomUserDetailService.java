c878f6c012447ddb654fda193cd03e9f1de33cd0
Mr-Ankit-kashyap <ankitkumar20224@gmail.com>
2024-09-19 18:15:45 +0530

    fixed some problom

--- src/main/java/com/api/securities/CustomUserDetailService.java
+++ src/main/java/com/api/securities/CustomUserDetailService.java
@@ -17,9 +17,9 @@ public class CustomUserDetailService implements UserDetailsService{
 	private UserRepository userRepository;
 
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		
-		User user = this.userRepository.findByEmail(username).orElseThrow(() -> new ResourceNotFoundException("user","email :" +username,0));
+		User user = this.userRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("user","email :" +email,0));
 		
 		return user;
 	}
